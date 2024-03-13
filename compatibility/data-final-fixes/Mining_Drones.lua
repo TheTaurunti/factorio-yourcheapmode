4caf91703013aa4dac8f6b8542a504cada95dac7
TheTaurunti <153017348+TheTaurunti@users.noreply.github.com>
2024-03-13 12:57:00 -0700

    added compat for mining drones

---
+++ compatibility/data-final-fixes/Mining_Drones.lua
@@ -0,0 +1,4 @@
+if mods["Mining_Drones"]
+then
+  table.insert(Mod_Excluded_Recipe_Categories, "mining-depot")
+end
--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -6,46 +6,14 @@ Mod_Ingredient_Equivalencies = {}
 
 table.insert(Mod_Ingredient_Equivalencies, { "water", "steam" })
 
--- Utils
-function Remove_Recipe_Result(recipe, result_name_to_remove)
-  local recipe_standard = recipe.normal or recipe
-  if (not recipe_standard.results) then return end
-
-  local new_results = {}
-  local removed_result = nil
-
-  for _, result in ipairs(recipe_standard.results) do
-    local name = result.name or result[1]
-
-    if (name == result_name_to_remove)
-    then
-      removed_result = result
-    else
-      table.insert(new_results, result)
-    end
-  end
-
-  recipe_standard.results = new_results
-  return removed_result
-end
-
-function Add_Recipe_Result(recipe, result_to_add)
-  local recipe_standard = recipe.normal or recipe
-  if (not recipe_standard.results) then return end
-
-  table.insert(recipe.results, result_to_add)
-end
-
-function Set_Recipe_Time(recipe, time)
-  local recipe_standard = recipe.normal or recipe
-
-  recipe_standard.energy_required = time
-end
+-- Util functions for editing recipes
+require("prototypes.recipe.Utils")
 
 -- Compatibility
 require("compatibility.data-final-fixes.FreightForwarding")
 require("compatibility.data-final-fixes.IntermodalContainers")
 require("compatibility.data-final-fixes.Krastorio2")
+require("compatibility.data-final-fixes.Mining_Drones")
 require("compatibility.data-final-fixes.SpaceExploration")
 
 -- Mod Logic
